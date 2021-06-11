7fd0c9f75015de7c3d6015462ff82ba818a9e234
ByungSoo Ye <69490984+DevelopSoo@users.noreply.github.com>
2021-06-11 15:42:16 +0900

    코어 모듈 학습

--- main.js
+++ main.js
@@ -10,8 +10,19 @@ let math = require('./math-tools.js');
 
 /* 모듈에서 객체로 한 번에 가져올 경우 */
 
-console.log(math.PI)
-console.log(math.add(1, 2));
-console.log(math.subtract(1, 2));
-console.log(math.multiply(1, 2));
-console.log(math.divide(1, 2));
+// console.log(math.PI)
+// console.log(math.add(1, 2));
+// console.log(math.subtract(1, 2));
+// console.log(math.multiply(1, 2));
+// console.log(math.divide(1, 2));
+
+const fs = require('fs')
+
+let fileList = fs.readdirSync('.');
+console.log(fileList)
+
+fs.writeFileSync('new', 'Hello Node.js')
+
+const os = require('os')
+
+console.log(os.cpus())
