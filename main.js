c076fc956772bd3e3e4ad2fc16b0eada232c0749
ByungSoo Ye <69490984+DevelopSoo@users.noreply.github.com>
2021-06-11 14:17:32 +0900

    객체를 이용하여 한 번에 가져오기

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let math = require('./math-tools.js');
 
-console.log(math.PI);
-console.log(math.author);
+/* 모듈에서 하나씩 가져올 경우 */
+// console.log(math.PI);
+// console.log(math.author);
+// console.log(math.add(1, 2));
+// console.log(math.test.date);
+// console.log(math.test.types)
+// math.test.printTypes()
+
+/* 모듈에서 객체로 한 번에 가져올 경우 */
+
+console.log(math.PI)
 console.log(math.add(1, 2));
-console.log(math.test.date);
-console.log(math.test.types)
-math.test.printTypes()
+console.log(math.subtract(1, 2));
+console.log(math.multiply(1, 2));
+console.log(math.divide(1, 2));
--- math-tools.js
+++ math-tools.js
@@ -1,21 +1,34 @@
-const PI = 3.14;
-let author = 'codeit teacher';
+/* 모듈에서 하나씩 가져올 경우 */
+// const PI = 3.14;
+// let author = 'codeit teacher';
 
-function add(a, b) {
-    return a+b;
-}
+// function add(a, b) {
+//     return a+b;
+// }
 
-let test = {
-    date: '2020-09-20',
-    types: ['safetyTest', 'performanceTest'],
-    printTypes() {
-        for (const i in this.types) {
-            console.log(this.types[i])
-        }
-    }
+// let test = {
+//     date: '2020-09-20',
+//     types: ['safetyTest', 'performanceTest'],
+//     printTypes() {
+//         for (const i in this.types) {
+//             console.log(this.types[i])
+//         }
+//     }
+// };
+
+// exports.PI = PI;
+// exports.author = author;
+// exports.add = add;
+// exports.test = test;
+
+/* 모듈에서 객체로 한 번에 가져올 경우 */
+
+let calculator = {
+    PI: 3.14,
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
 };
 
-exports.PI = PI;
-exports.author = author;
-exports.add = add;
-exports.test = test;
+module.exports = calculator;
