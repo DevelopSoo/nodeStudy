7b2ca8ee159c81a5d882a1a24818d5041deab31f
ByungSoo Ye <69490984+DevelopSoo@users.noreply.github.com>
2021-06-12 00:31:20 +0900

    간단한 외부 통신 만들기

--- main.js
+++ main.js
@@ -1,50 +1,8 @@
-let math = require('./math-tools.js');
+const http = require('http');
 
-/* 모듈에서 하나씩 가져올 경우 */
-// console.log(math.PI);
-// console.log(math.author);
-// console.log(math.add(1, 2));
-// console.log(math.test.date);
-// console.log(math.test.types)
-// math.test.printTypes()
-
-/* 모듈에서 객체로 한 번에 가져올 경우 */
-
-// console.log(math.PI)
-// console.log(math.add(1, 2));
-// console.log(math.subtract(1, 2));
-// console.log(math.multiply(1, 2));
-// console.log(math.divide(1, 2));
-
-/* 코어 모듈 */
-// const fs = require('fs')
-
-// let fileList = fs.readdirSync('.');
-// console.log(fileList)
-
-// fs.writeFileSync('new', 'Hello Node.js')
-
-// const os = require('os')
-
-// console.log(os.cpus())
-
-/* 서드파티 모듈 */
-// const cowsay = require('cowsay')
-
-// console.log(cowsay.say({
-//     text: "I Love Typescript"
-// }))
-
-
-/* 비동기 프로그램과 EventEmitter */
-const EventEmitter = require('events');
-
-const myEmitter = new EventEmitter();
-
-const obj = {type: 'text', data: 'Hello Codeit', date: '2020-09-01'};
-
-myEmitter.on('test', (info) => {
-    console.log(info);
+let server = http.createServer(function (request, response) {
+    response.end('<h1>Hello World!</h1>')
 });
+server.listen(3000);
 
-myEmitter.emit('test', obj);
+
